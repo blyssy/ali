53054e92fc06de0f14b5683f4c6f190e7ecaba92
Brian Lyssy <brian.lyssy@nwdc.hpc.mil>
2015-05-07 18:56:07 -0500

    added initial row editing of tasks in the bid request

--- packages/custom/bids/public/controllers/bidrequestedit.js
+++ packages/custom/bids/public/controllers/bidrequestedit.js
@@ -4,6 +4,89 @@ angular.module('mean.bids').controller('BidRequestEditController', ['$scope', 'U
   function($scope, Users, Global, Bids, $filter, NGTableParams, $sce, toaster, NotifyService, BidRequestEdit) {
     $scope.global = Global;
 
+    $scope.init = function() {
+    	$scope.bid = BidRequestEdit.get();
+
+    	$scope.bid_menu = [];
+
+    	var menu = $scope.bid.project_plan;
+
+    	if(menu.single.length) {
+    		var elevations = [];
+    		$scope.construction_single = true;
+
+    		menu.single.forEach(function(object, Idx){
+    			elevations = object.elevations.split(',');
+
+				elevations.forEach(function(subitem, ii){
+					subitem = subitem.replace(/\s/g, '');
+					$scope.bid_menu.push({
+						item: object.number + subitem
+					});
+				});
+    		});
+
+    	} else if(menu.multi.interior.length || menu.multi.exterior.length){
+    		$scope.construction_multi = true;
+    		menu.multi.interior.forEach(function(item, index){
+    			menu.multi.exterior.forEach(function(subitem, ii){
+    				$scope.bid_menu.push({
+						item: item.item + subitem.item
+					});
+    			});
+    		});
+    	} else {
+    		alert('unknown construction type');
+    	}
+
+    	var bid_tasks = [];
+    	var current_bid_index = 0;
+
+    	Users.get({ userId: $scope.global.user._id}, function(user) {
+	    	$scope.bid.task_list.forEach(function(task, index){
+	    		var code = $scope.getTradeCode(user.trade[0]);
+	    		//console.log('code value ' + code + ' task code value ' + task.trade);
+	    		if( task.trade === code ){
+	    			bid_tasks.push(task);
+	    			$scope.bid.bidding_trades.forEach(function(bid, ii){
+	    				if(bid.trade === user.trade[0]) {
+	    					console.log('this is where we need to add to the list for actual bid numbers ' + code);
+	    					//dummy values for now.  need to pull from the first bid index for the bidder
+	    					bid_tasks[current_bid_index].task_labor = index;
+	    					bid_tasks[current_bid_index].task_equipment = index;
+	    					bid_tasks[current_bid_index].task_material = index;
+	    					current_bid_index = current_bid_index + 1;
+	    				}
+	    			});
+	    		}
+	    	});
+
+	    	$scope.bid_total = '2000';
+	    	$scope.bid_direct_labor = '900';
+
+	    	var data = bid_tasks;
+	        $scope.bidTasksTableParams = new NGTableParams({
+	            page: 1,
+	            count: 10
+	        },{
+	            total: data.length,
+	            getData: function($defer, params) {
+	                params.total(data.length);
+	                var orderedData = params.sorting()?$filter('orderBy')(data, params.orderBy()):data;
+	                $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+	            }
+	        });
+	    });
+    };
+
+    $scope.onSelect = function() {
+    	console.log('in the onSelect function ' + $scope.current_plan.item);
+    };
+
+    $scope.setTaskEditId =  function(pid) {
+        $scope.taskEditId = pid;
+    };
+
     $scope.getTradeCode = function(trade) {
     	var value;
     	switch(trade){
@@ -69,75 +152,6 @@ angular.module('mean.bids').controller('BidRequestEditController', ['$scope', 'U
     		break;
     	}
     	return value;
-    };
-
-    $scope.init = function() {
-    	$scope.bid = BidRequestEdit.get();
-
-    	$scope.bid_menu = [];
-
-    	var menu = $scope.bid.project_plan;
-
-    	if(menu.single.length) {
-    		var elevations = [];
-    		$scope.construction_single = true;
-
-    		menu.single.forEach(function(object, Idx){
-    			elevations = object.elevations.split(',');
-
-				elevations.forEach(function(subitem, ii){
-					subitem = subitem.replace(/\s/g, '');
-					$scope.bid_menu.push({
-						item: object.number + subitem
-					});
-				});
-    		});
-
-    	} else if(menu.multi.interior.length || menu.multi.exterior.length){
-    		$scope.construction_multi = true;
-    		menu.multi.interior.forEach(function(item, index){
-    			menu.multi.exterior.forEach(function(subitem, ii){
-    				$scope.bid_menu.push({
-						item: item.item + subitem.item
-					});
-    			});
-    		});
-    	} else {
-    		alert('unknown construction type');
-    	}
-
-    	var bid_tasks = [];
-
-    	Users.get({ userId: $scope.global.user._id}, function(user) {
-	    	$scope.bid.task_list.forEach(function(task, index){
-	    		var code = $scope.getTradeCode(user.trade[0]);
-	    		//console.log('code value ' + code + ' task code value ' + task.trade);
-	    		if( task.trade === code ){
-	    			bid_tasks.push(task);
-	    		}
-	    	});
-
-	    	var data = bid_tasks;
-	        $scope.bidTasksTableParams = new NGTableParams({
-	            page: 1,
-	            count: 10
-	        },{
-	            total: data.length,
-	            getData: function($defer, params) {
-	                params.total(data.length);
-	                var orderedData = params.sorting()?$filter('orderBy')(data, params.orderBy()):data;
-	                $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
-	            }
-	        });
-	    });
-    };
-
-    $scope.onSelect = function() {
-    	console.log('in the onSelect function ' + $scope.current_plan.item);
-    };
-
-    $scope.setTaskEditId =  function(pid) {
-        $scope.taskEditId = pid;
     };
   }
 ]);
