b4fb238709bd95145e43533cb1f7d33f2ef833c7
Brian Lyssy <lyssybrian2@gmail.com>
2015-06-28 13:03:20 -0500

    change database and server

--- config/env/production.js
+++ config/env/production.js
@@ -2,8 +2,7 @@
 
 module.exports = {
   //db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/heroku_app35133957',
-  //db: 'mongodb://heroku_app35133957:3t7kom4vg7jaa8eskl02r924nn@ds053877.mongolab.com:53877/heroku_app35133957',
-  db: 'mongodb://blyssy:blyss001@ds031751.mongolab.com:31751/heroku_scxc0vzl',
+  db: 'mongodb://heroku_app35133957:3t7kom4vg7jaa8eskl02r924nn@ds053877.mongolab.com:53877/heroku_app35133957',
   /**
    * Database options that will be passed directly to mongoose.connect
    * Below are some examples.
